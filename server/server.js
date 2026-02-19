acd36ea4127262144d4f02478e7964554955835b
Swanand <swanand.siknis007@gmail.com>
2026-02-19 19:14:13 +0530

    Fix PORT fir Render Deployment

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/medicines", medicineRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
